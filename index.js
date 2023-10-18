1aff4b38aa19dde695edabe29470ba873f8f655e
Taona Madawo <synthesis.co.za>
2023-10-18 15:34:42 +0200

    unblocking errors

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var port = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
-
+//test comment
 app.get('/', function (req, res) {
   res.send("Hello GitHub Actions Streaming - github-actions-deploy-docker-to-ec2");
 });
